4706be1011df356efaa82f2f56ad4228bff2f4aa
nedkia <kiamen2@tcnj.edu>
2024-04-17 22:04:39 -0400

    yeet

--- responses.py
+++ responses.py
@@ -6,16 +6,16 @@ def get_response(user_input: str) -> str:
 	if lowered == '':
 		return 'Well, you\'re awfully silent...'
 	elif 'hello' in lowered:
-		return 'Hello there!'
+		return 'yo '
 	elif 'how are you' in lowered:
-		return 'Good, thanks!'
+		return 'not bad!'
 	elif 'bye' in lowered:
-		return 'Scram Kid!'
+		return 'pce btich!'
 	elif 'who is king' in lowered:
-		return 'Not Ned that\'s for sure'
+		return 'NED is KING'
 	elif 'roll dice' in lowered:
 		return f'You rolled: {randint(1, 6)}'
 	else:
 		return choice(['I do not understand...',
 			'What are you talking about?',
-			'Do you mind rephrasing that?'])
+			'Do you mind rephrasing that?', 'tf you say to me?','idk bro', 'idk but zach is a bitch frfr','damn he thic','yeah','nah','sure','LMAOO??','stfu','bet lets do it','oh snap!','YIPEEREEEEE','I shit myself','REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE']
